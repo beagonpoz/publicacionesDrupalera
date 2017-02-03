c4f9201dbaeb9595a76569e56ace1aa595acc88d
Pepe Garcia Garcia <pgarciag93@gmail.com>
2017-02-03 15:02:13 +0100

    Otro commit pa la bea

--- prueba.php
+++ prueba.php
@@ -1,19 +1,18 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Habilitar la configuración de propiedades del nodo en view modes en Drupal 8.
-|--------------------------------------------------------------------------
-|
-| El objetivo del snippet es habilitar la configuración vía interfaz de algunas
-| propiedades de los nodos en los diferentes modos de visualización (view modes)
-| de los tipos de contenido, tarea que por defecto no podremos realizar.
-| Esto nos permitirá, entre otras cosas, modificar su posición dentro del
-| contenido a mostrar o aplicar un formatter diferente.
-|
-| En este caso, el snippet descrito a continuación, habilitará el campo
-| fecha de creación (created) para su configuración en diferentes view modes.
-|
+*-------------------------------------------------------------------------------
+* Habilitar la configuración de propiedades del nodo en view modes en Drupal 8.
+*-------------------------------------------------------------------------------
+*
+* El objetivo del snippet es habilitar la configuración vía interfaz de algunas
+* propiedades de los nodos en los diferentes modos de visualización (view modes)
+* de los tipos de contenido, tarea que por defecto no podremos realizar.
+* Esto nos permitirá, entre otras cosas, modificar su posición dentro del
+* contenido a mostrar o aplicar un formatter diferente.
+*
+* En este caso, el snippet descrito a continuación, habilitará el campo
+* fecha de creación (created) para su configuración en diferentes view modes.
 */
 
 /**
